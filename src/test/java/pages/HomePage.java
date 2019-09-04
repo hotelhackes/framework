e00f762f6abd7121d64dad1d50a50da60c7b567b
martijnvanes <51001282+martijnvanes@users.noreply.github.com>
2019-09-04 18:24:36 +0200

    upload

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -10,77 +10,93 @@ import java.util.List;
 
 public class HomePage extends BasePage {
 
-    @FindBy(css = "li#header_link_contact > a")
-    private WebElement contactButton;
+    @FindBy(id = "next")
+    private WebElement nextButton;
 
-    @FindBy(css = "[class='login']")
-    private WebElement logInButton;
+    public void setNextButton() {
 
-    @FindBy(css = "[title='Home']")
-    private WebElement homeButton;
+       nextButton.click();
 
-    @FindBy(css = "[name='search_query']")
-    private WebElement searchField;
+    }
 
-    @FindBy(css = "[name='submit_search']")
-    private WebElement submitSearch;
+    @FindBy(id= "closeModal")
+    private WebElement closeButton;
 
-    @FindBy(css = "#tags_block_left > div")
-    private WebElement tagContainer;
+    public void setCloseButton() {
+        closeButton.click();
+    }
 
-    private By accountDetailsButton = By.cssSelector("[class='account']");
+    //    @FindBy(css = "li#header_link_contact > a")
+//    private WebElement contactButton;
+//
+//    @FindBy(css = "[class='login']")
+//    private WebElement logInButton;
+//
+//    @FindBy(css = "[title='Home']")
+//    private WebElement homeButton;
+//
+//    @FindBy(css = "[name='search_query']")
+//    private WebElement searchField;
+//
+//    @FindBy(css = "[name='submit_search']")
+//    private WebElement submitSearch;
+//
+//    @FindBy(css = "#tags_block_left > div")
+//    private WebElement tagContainer;
+//
+//    private By accountDetailsButton = By.cssSelector("[class='account']");
 
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
-    public void openContactUsPage() {
-        contactButton.click();
-    }
+//    public void openContactUsPage() {
+//        contactButton.click();
+//    }
+//
+//    public void clickLogIn() {
+//        logInButton.click();
+//    }
+//
+//    public void goToHomePage() {
+//        homeButton.click();
+//    }
+//
+//    public String getAccountName() {
+//        List<WebElement> el = driver.findElements(accountDetailsButton);
+//        if (el.size() != 0) {
+//            for (WebElement element : el) {
+//                return element.getText();
+//            }
+//        }
+//        return "account details button is not present on the page";
+//    }
 
-    public void clickLogIn() {
-        logInButton.click();
-    }
-
-    public void goToHomePage() {
-        homeButton.click();
-    }
-
-    public String getAccountName() {
-        List<WebElement> el = driver.findElements(accountDetailsButton);
-        if (el.size() != 0) {
-            for (WebElement element : el) {
-                return element.getText();
-            }
-        }
-        return "account details button is not present on the page";
-    }
-
-    private WebElement getTagsBlock() {
-        return tagContainer;
-    }
-
-    public Boolean tagPresent(String tagName) {
-        List<String> tagsList = new ArrayList<>();
-        List<WebElement> tagsBlock = getTagsBlock().findElements(By.cssSelector("a"));
-        for (WebElement webElement : tagsBlock) {
-            tagsList.add(webElement.getText());
-        }
-        return tagsList.contains(tagName);
-    }
-
-    public List<WebElement> getProducts() {
-        WebElement productList = driver.findElement(By.cssSelector("#product_list"));
-        return productList.findElements(By.cssSelector("a.product-name"));
-    }
-
-    public List<WebElement> getStocks() {
-        WebElement productList = driver.findElement(By.cssSelector("#product_list"));
-        return productList.findElements(By.cssSelector(".availability"));
-    }
-
-    public void searchProduct(String searchInput) {
-        searchField.sendKeys(searchInput);
-        submitSearch.click();
-    }
+//    private WebElement getTagsBlock() {
+//        return tagContainer;
+//    }
+//
+//    public Boolean tagPresent(String tagName) {
+//        List<String> tagsList = new ArrayList<>();
+//        List<WebElement> tagsBlock = getTagsBlock().findElements(By.cssSelector("a"));
+//        for (WebElement webElement : tagsBlock) {
+//            tagsList.add(webElement.getText());
+//        }
+//        return tagsList.contains(tagName);
+//    }
+//
+//    public List<WebElement> getProducts() {
+//        WebElement productList = driver.findElement(By.cssSelector("#product_list"));
+//        return productList.findElements(By.cssSelector("a.product-name"));
+//    }
+//
+//    public List<WebElement> getStocks() {
+//        WebElement productList = driver.findElement(By.cssSelector("#product_list"));
+//        return productList.findElements(By.cssSelector(".availability"));
+//    }
+//
+//    public void searchProduct(String searchInput) {
+//        searchField.sendKeys(searchInput);
+//        submitSearch.click();
+//    }
 }
--- src/test/java/testshoptests/structured/ContactFormTest.java
+++ src/test/java/testshoptests/structured/ContactFormTest.java
@@ -12,19 +12,29 @@ public class ContactFormTest extends TestShopScenario {
 
         // Goto contact us page
         HomePage homePage = new HomePage(driver);
-        homePage.openContactUsPage();
 
-        // Fill contact form and send it
-        ContactFormPage contactFormPage = new ContactFormPage(driver);
-        contactFormPage.sendFilledInContactForm(
-                ContactFormPage.SubjectHeadings.WEBMASTER,
-                "test@test.com",
-                "0987654321",
-                "Some message");
 
-        // Verify message was sent
-        Assert.assertEquals("Your message has been successfully sent to our team.",
-                contactFormPage.getAlertMessage());
+        homePage.setNextButton();
+        homePage.setNextButton();
+        homePage.setNextButton();
+        homePage.setNextButton();
+//        homePage.setNextButton();
+        homePage.setCloseButton();
+
+
+
+//
+//        // Fill contact form and send it
+//        ContactFormPage contactFormPage = new ContactFormPage(driver);
+//        contactFormPage.sendFilledInContactForm(
+//                ContactFormPage.SubjectHeadings.WEBMASTER,
+//                "test@test.com",
+//                "0987654321",
+//                "Some message");
+//
+//        // Verify message was sent
+//        Assert.assertEquals("Your message has been successfully sent to our team.",
+//                contactFormPage.getAlertMessage());
 
     }
 
